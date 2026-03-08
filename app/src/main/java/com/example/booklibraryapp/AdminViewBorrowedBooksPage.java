72bb7d18e1b782ec4183f82f3e9fe3ecbf3c230d
Hassan Koubaissi <hassk9>
2026-03-08 08:31:17 -0400

    Implemented admin pages

--- app/src/main/java/com/example/booklibraryapp/AdminViewBorrowedBooksPage.java
+++ app/src/main/java/com/example/booklibraryapp/AdminViewBorrowedBooksPage.java
@@ -1,64 +1,42 @@
 package com.example.booklibraryapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminViewBorrowedBooksPage#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 public class AdminViewBorrowedBooksPage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ListView listView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AdminViewBorrowedBooksPage() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminViewBorrowedBooksPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminViewBorrowedBooksPage newInstance(String param1, String param2) {
-        AdminViewBorrowedBooksPage fragment = new AdminViewBorrowedBooksPage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    String[] borrowedBooks = {
+            "The History Boys - Borrowed by User1",
+            "What If It's Us - Borrowed by User2",
+            "The Liar's Dictionary - Borrowed by User3"
+    };
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_view_borrowed_books_page, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_admin_view_borrowed_books_page, container, false);
+
+        listView = view.findViewById(R.id.borrowedList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_list_item_1,
+                borrowedBooks
+        );
+
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
