3a32d33119d1a29cb706ab7df87eca040ef7180d
Tom Tooma <ttooma@umich.edu>
2026-03-08 21:13:50 -0400

    Adding to admin view and user view pages. Adding temporary if logic to log into user vs admin.

--- app/src/main/java/com/example/booklibraryapp/LoginPage.java
+++ app/src/main/java/com/example/booklibraryapp/LoginPage.java
@@ -1,13 +1,19 @@
 package com.example.booklibraryapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.booklibraryapp.databinding.LoginPageBinding;
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.concurrent.ExecutorService;
@@ -18,29 +24,34 @@ public class LoginPage extends Fragment {
     java.sql.Connection connection;
     private LoginPageBinding binding;
 
+    private TextInputEditText userInputUsername;
+
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     )
     {
-
         binding = LoginPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        userInputUsername = view.findViewById(R.id.inputLoginUsername);
         binding.btnLogin.setOnClickListener(v -> {
-            // Put your code here:
-            // For example:
-            // Toast.makeText(requireContext(), "Button clicked!", Toast.LENGTH_SHORT).show();
-
-            // If you still want to navigate:
-            NavHostFragment.findNavController(LoginPage.this)
-                    .navigate(R.id.action_LoginPage_to_userPage);
+            //if logic for navigating to admin vs user page, will add db elements later
+            if (userInputUsername.getText().toString().equals("admin")){
+                NavHostFragment.findNavController(LoginPage.this)
+                        .navigate(R.id.action_LoginPage_to_AdminPage);
+            } else if (userInputUsername.getText().toString().isBlank()) {
+                NavHostFragment.findNavController(LoginPage.this)
+                        .navigate(R.id.action_LoginPage_to_userPage);
+            }
+            else {
+                NavHostFragment.findNavController(LoginPage.this)
+                        .navigate(R.id.action_LoginPage_to_userPage);
+            }
         });
 
         binding.btnLoginCreateAccount.setOnClickListener(v -> {
