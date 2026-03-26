1f63f14f111d706afde133d91ad975ad766a03a9
bpisaleumich <bpisale@umich.edu>
2026-03-26 08:24:57 -0400

    Added option to search user account using search bar

--- app/src/main/java/com/example/booklibraryapp/AdminAccountsInfoPage.java
+++ app/src/main/java/com/example/booklibraryapp/AdminAccountsInfoPage.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import java.util.List;
 
 public class AdminAccountsInfoPage extends Fragment
 {
     ListView userSearchResult;
+    SearchView userSearchInput;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -25,9 +27,25 @@ public class AdminAccountsInfoPage extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_admin_accounts_info_page, container, false);
         userSearchResult = view.findViewById(R.id.userSearchResult);
+        userSearchInput = view.findViewById(R.id.userSearchInput);
+
         List<String> usernamesQuery = QueryConnectorPlusHelper.getUsernamesQuery();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, usernamesQuery);
         userSearchResult.setAdapter(adapter);
+
+        userSearchInput.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
         userSearchResult.setOnItemClickListener((parent, view1, position, id) ->
         {
             String userSelected = (String) parent.getItemAtPosition(position);
